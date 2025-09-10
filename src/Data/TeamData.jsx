3fa04bdd88c06c0d10ab41d4c8af4f247164b556
unknown <misbahudinzaki07@gmail.com>
2025-09-10 09:33:23 +0700

    cleaning code

--- src/Data/TeamData.jsx
+++ src/Data/TeamData.jsx
@@ -1,68 +1,68 @@
 export const teamData = [
-  {
-    id: 1,
-    name: "Jordan Lee",
-    title: "Head of Creative",
-    image: "/assets/image/team-3.jpg",
-    socials: {
-      facebook: "https://facebook.com",
-      instagram: "https://instagram.com",
-      linkedin: "https://linkedin.com",
-    },
-  },
-  {
-    id: 2,
-    name: "Chloe Tan",
-    title: "Senior SEO Specialist",
-    image: "/assets/image/team-2.jpg",
-    socials: {
-      facebook: "https://facebook.com",
-      instagram: "https://instagram.com",
-      linkedin: "https://linkedin.com",
-    },
-  },
-  {
-    id: 3,
-    name: "Daniel Cruz",
-    title: "Performance Marketing Lead",
-    image: "/assets/image/team-1.jpg",
-    socials: {
-      facebook: "https://facebook.com",
-      instagram: "https://instagram.com",
-      linkedin: "https://linkedin.com",
-    },
-  },
-  {
-    id: 4,
-    name: "Olivia Bennett",
-    title: "Creative Director",
-    image: "/assets/image/team-4.jpg",
-    socials: {
-      facebook: "https://facebook.com",
-      instagram: "https://instagram.com",
-      linkedin: "https://linkedin.com",
-    },
-  },
-  {
-    id: 5,
-    name: "Daniel White",
-    title: "Client Success Manager",
-    image: "/assets/image/team-6.jpg",
-    socials: {
-      facebook: "https://facebook.com",
-      instagram: "https://instagram.com",
-      linkedin: "https://linkedin.com",
-    },
-  },
-  {
-    id: 6,
-    name: "Chloe Ramirez",
-    title: "Social Media Manager",
-    image: "/assets/image/team-5.jpg",
-    socials: {
-      facebook: "https://facebook.com",
-      instagram: "https://instagram.com",
-      linkedin: "https://linkedin.com",
-    },
-  },
+	{
+		id: 1,
+		name: "Jordan Lee",
+		title: "Head of Creative",
+		image: "/assets/image/team-3.jpg",
+		socials: {
+		facebook: "https://facebook.com",
+		instagram: "https://instagram.com",
+		linkedin: "https://linkedin.com",
+		},
+	},
+	{
+		id: 2,
+		name: "Chloe Tan",
+		title: "Senior SEO Specialist",
+		image: "/assets/image/team-2.jpg",
+		socials: {
+		facebook: "https://facebook.com",
+		instagram: "https://instagram.com",
+		linkedin: "https://linkedin.com",
+		},
+	},
+	{
+		id: 3,
+		name: "Daniel Cruz",
+		title: "Performance Marketing Lead",
+		image: "/assets/image/team-1.jpg",
+		socials: {
+		facebook: "https://facebook.com",
+		instagram: "https://instagram.com",
+		linkedin: "https://linkedin.com",
+		},
+	},
+	{
+		id: 4,
+		name: "Olivia Bennett",
+		title: "Creative Director",
+		image: "/assets/image/team-4.jpg",
+		socials: {
+		facebook: "https://facebook.com",
+		instagram: "https://instagram.com",
+		linkedin: "https://linkedin.com",
+		},
+	},
+	{
+		id: 5,
+		name: "Daniel White",
+		title: "Client Success Manager",
+		image: "/assets/image/team-6.jpg",
+		socials: {
+		facebook: "https://facebook.com",
+		instagram: "https://instagram.com",
+		linkedin: "https://linkedin.com",
+		},
+	},
+	{
+		id: 6,
+		name: "Chloe Ramirez",
+		title: "Social Media Manager",
+		image: "/assets/image/team-5.jpg",
+		socials: {
+		facebook: "https://facebook.com",
+		instagram: "https://instagram.com",
+		linkedin: "https://linkedin.com",
+		},
+	},
 ];
